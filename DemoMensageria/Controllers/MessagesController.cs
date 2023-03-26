933d8e5481431e9074d2a1eb9e40d1197bb47032
Leandro <leandro.almeida@uvvnet.com.br>
2023-03-26 11:09:40 -0300

    aplicando os dados dew conexão

---
+++ DemoMensageria/Controllers/MessagesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace DemoMensageria.Controllers
+{
+    [Route("api/messages")]
+    public class MessagesController : ControllerBase
+    {
+        private const string QUEUE_NAME = "messages";
+        private readonly ConnectionFactory _factory;
+
+        public MessagesController()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+        }
+    }
+}
